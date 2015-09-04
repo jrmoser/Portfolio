be812851bb59899469cd219dc908e39ce03c6584
jrmoser <jarodmoser@gmail.com>
2015-09-04 12:38:41 -0600

    Dynamic header on scroll

--- Javasript/script.js
+++ Javasript/script.js
@@ -1,4 +1,13 @@
 /**
  * Created by jarodmoser on 9/4/15.
  */
-document.write("javasript is working");
+$(function() {
+	$(window).scroll(function() {
+
+		if ($(window).scrollTop() >= 100) {
+			$(".header").addClass('smaller');
+		} else {
+			$(".header").removeClass("smaller");
+		}
+	});
+});
